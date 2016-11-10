1f9192f1f9a3c3f2aa45fce19a6d6ad645d324b7
Kairat <talantbekov123@gmail.com>
2016-11-10 18:07:16 +0600

    Trying to parse web page using goquery, but NOT successfull

--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
+	"github.com/PuerkitoBio/goquery"
+	
 	"net/http"
+	"fmt"
+	"log"
+	//"io/ioutil"
 )
 
 
@@ -14,8 +19,23 @@ func main() {
 	m.Use(render.Renderer())
 
 	m.Get("/", func(r render.Render) {
+
+		doc, err := goquery.NewDocument("http://www.ikea.com/ru/ru/catalog/allproducts/")
+        if err != nil {
+                log.Fatal(err)
+        }
+
+        doc.Find(".productCategoryContainerWraper").Each(func(i int, s *goquery.Selection) {
+                fmt.Println(i)
+       
+                title := s.Find("a").Text()
+                link, _ := s.Find("a").Attr("href")
+                fmt.Println(title)
+                fmt.Println(link)
+        })
+
 		r.HTML(http.StatusOK, "index", nil)	
 	})
 
-	m.Run()
+	m.RunOnAddr(":8094")
 }
